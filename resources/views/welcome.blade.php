a7de3957bba6b05200a9c76de1ecb3dd3e832c94
AkmalKhairi <akmalkhairi24@gmail.com>
2021-12-05 18:15:49 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>MKhairi</title>
+    <link rel="icon" type="image/x-icon" href="/img/favicon1.png" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="{{asset('../css/styles.css')}}" rel="stylesheet" />
+</head>
+<body id="page-top">
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="sideNav">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">Porfolio MKhairi</span>
+        <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="/img/malkhairi.jpg" alt="..." /></span>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Home</a></li>
+            <!--<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>-->
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#project">Projects</a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
+        </ul>
+    </div>
+</nav>
+<!-- Page Content-->
+<div class="container-fluid p-0">
+    <!-- About-->
+    <section class="resume-section" id="about">
+        <div class="resume-section-content">
+            <h1 class="mb-0">
+                Muhd
+                <span class="text-info">Akmal Khairi</span>
+            </h1>
+            <div class="subheading mb-5">
+                <!--3542 Berry Street · Cheyenne Wells, CO 80810 · (317) 585-8468 ·-->
+                <a href="mailto:akmalkhairi24@gmail.com">akmalkhairi24@gmail.com</a>
+            </div>
+            <p class="lead mb-5"></p>
+            <h3 class="mb-5">
+                Welcome!<br> Here are some info about me
+            </h3>
+            {{--<p class="lead mb-5">&#128313  <br> &#128313  <br> &#128313 </p>--}}
+            <ul class="fa-ul mb-5">
+                <li>
+                    <span class="fa-li"><i class="fas fa-bug"></i></span>
+                    Loves to Code & Explore New Tech Things
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-futbol"></i></span>
+                    Manchester United Fans
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fab fa-apple"></i></span>
+                    Apple Product Enthusiast
+                </li>
+            </ul>
+            <div class="social-icons">
+                <a class="social-icon" href="https://www.linkedin.com/in/muhammad-akmal-khairi-82a722199/"><i class="fab fa-linkedin-in"></i></a>
+                <a class="social-icon" href="https://github.com/AkmalKhairi"><i class="fab fa-github"></i></a>
+                <!--<a class="social-icon" href="#!"><i class="fab fa-twitter"></i></a>
+                <a class="social-icon" href="#!"><i class="fab fa-facebook-f"></i></a>-->
+            </div>
+        </div>
+    </section>
+    <!--<hr class="m-0" />
+    &lt;!&ndash; Experience&ndash;&gt;
+    <section class="resume-section" id="experience">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Experience</h2>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Senior Web Developer</h3>
+                    <div class="subheading mb-3">Intelitec Solutions</div>
+                    <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">March 2013 - Present</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Web Developer</h3>
+                    <div class="subheading mb-3">Intelitec Solutions</div>
+                    <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">December 2011 - March 2013</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Junior Web Designer</h3>
+                    <div class="subheading mb-3">Shout! Media Productions</div>
+                    <p>Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.</p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">July 2010 - December 2011</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Web Design Intern</h3>
+                    <div class="subheading mb-3">Shout! Media Productions</div>
+                    <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">September 2008 - June 2010</span></div>
+            </div>
+        </div>
+    </section>-->
+    <hr class="m-0" />
+    <!-- Education-->
+    <section class="resume-section" id="education">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Education</h2>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Universiti Teknikal Malaysia Melaka</h3>
+                    <div class="subheading mb-3">Bachelor of Computer Science (Software Development)</div>
+                    <p>GPA: 3.63</p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">February 2020 - July 2022</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0"> Universiti Teknikal Malaysia Melaka</h3>
+                    <div class="subheading mb-3">Diploma in Information and Communication Technology</div>
+                    <p>GPA: 2.94</p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">July 2017 - January 2020</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Sekolah Menengah Kebangsaan Alam Damai</h3>
+                    <div class="subheading mb-3">Art Stream</div>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">January 2012 - December 2016</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Sekolah Kebangsaan Seri Anggerik</h3>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">January 2006 - December 2011</span></div>
+            </div>
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Skills-->
+    <section class="resume-section" id="skills">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Skills</h2>
+            <div class="subheading mb-3">Programming Languages & Tools</div>
+            <ul class="list-group list-group-horizontal dev-icons">
+                <li class="list-group-item flex-fill h2"><i class="fab fa-php"></i> PHP</li>
+                <li class="list-group-item flex-fill h2"><i class="fab fa-html5"></i> HTML </li>
+                <li class="list-group-item flex-fill h2"><i class="fab fa-css3-alt"></i> CSS </li>
+                <li class="list-group-item flex-fill h2"><i class="fab fa-java"></i> JAVA</li>
+            </ul>
+            <ul class="list-group list-group-horizontal dev-icons">
+                <li class="list-group-item flex-fill h2"><i class="fab fa-js-square" ></i> JSCRIPT</li>
+                <li class="list-group-item flex-fill h2"><i class="fab fa-android" ></i> ANDROID</li>
+                <li class="list-group-item flex-fill h2"><i class="fab fa-python" ></i> PYTHON </li>
+                <li class="list-group-item flex-fill h2"><i class="fab fa-laravel" ></i> LARAVEL</li>
+            </ul>
+
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Interests-->
+    <section class="resume-section" id="project">
+        <div class="resume-section-content">
+            <h2 class="mb-5 mt-0">PROJECTS</h2>
+            <div class="subheading mb-3">PAST PROJECTS DEVELOPED</div>
+            <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    {{--<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>--}}
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active flex-fill" data-bs-interval="10000">
+                        <img src="/img/eundi.png" class="d-block w-200" alt="...">
+                        <div class="carousel-caption d-none d-lg-block">
+                            <p> <a href="https://github.com/AkmalKhairi/e-undiv2" class="text-secondary"> E-Undi</a> purpose is to create a system that is easy, clear, and efficient for the end user to use during the election in these Pandemic Covid-19 season.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item flex-fill" data-bs-interval="10000">
+                        <img src="/img/vms.png" class="d-block w-200" alt="...">
+                        <div class="carousel-caption d-none d-lg-block">
+                            <p><a href="https://github.com/AkmalKhairi/Premise-Visitor-Monitor-System" class="text-secondary">Visitor Monitoring System </a> was developed by my team during Workshop 2 to monitors
+                                visitors entering and exit every premises by using IoT Technology during Pandemic Covid-19 season.</p>
+                        </div>
+                    </div>
+                    {{--<div class="carousel-item flex-fill">
+                        <img src="/img/eundi.png" class="d-block w-auto h-200" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>--}}
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Awards-->
+    <section class="resume-section" id="awards">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Awards & Certifications</h2>
+            <ul class="fa-ul mb-0">
+                <li>
+                    <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                    1
+                    <sup>st</sup>
+                    Place - Sukan Piala Dekan 2020
+
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                    2
+                    <sup>nd</sup>
+                    Place - Virtual Research and Innovation Exhibition UniMAP (EREKA) 2021
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                    The Code 2018: Inter-Varsity Programming Contest
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                    IEEE Malaysia Final Year Project (FYP) Competition 2021
+                </li>
+            </ul>
+        </div>
+    </section>
+</div>
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="{{asset('../js/scripts.js')}}"></script>
+</body>
+</html>
